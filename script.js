27a5cfbd961617cae981a058449f0b5fffe3ceb4
Alfalih <112360977+alfalih@users.noreply.github.com>
2024-10-07 14:17:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,33 @@ const darkmode = "dark-mode.css";
 const lightmode = "light-mode.css";
 
 const theme = document.getElementById("theme-mode");
+const checked = document.getElementById("theme-switcher");
+var toogle = document.getElementById('theme-switcher');
+
+let modenow = sessionStorage.getItem('modenow')
+
+if(!modenow){
+    sessionStorage.setItem('modenow',lightmode);
+}
+if(modenow === darkmode){
+    theme.setAttribute("href", darkmode);
+    sessionStorage.setItem('modenow',darkmode);
+    toogle.checked = true;
+}
+
 function switchMode(){
-    if(theme.getAttribute("href")===lightmode){
+    modenow = sessionStorage.getItem('modenow')
+    if(toogle.checked){
         theme.setAttribute("href", darkmode);
+        sessionStorage.setItem('modenow',darkmode);  
+        alert(cb.value);
     } else{
         theme.setAttribute("href", lightmode);
+        sessionStorage.setItem('modenow',lightmode);
+        alert(cb.value);
     }
+    e.preventDefault();
 }
+
+
+
